abe4a685361545af408eb1e846440acbcbd556e9
achuasokan <achuasokan555@gmail.com>
2025-12-10 01:27:42 +0530

    chore: added a folder for teh constants values int eh application by placing all the url of both backend and frontend ina separate folder and in a file

---
+++ src/constants/api/auth.ts
@@ -0,0 +1,9 @@
+export const AUTH_ENDPOINTS = {
+    
+    SIGNUP: "/auth/signup",
+    VERIFY_OTP: "/auth/verify-otp",
+    RESEND_OTP: "/auth/resend-otp",
+    LOGIN: "/auth/login",
+    LOGOUT: "/auth/logout",
+    
+} as const;
---
+++ src/constants/routes/auth.routes.ts
@@ -0,0 +1,7 @@
+export const AUTH_ROUTES = {
+
+    LOGIN: "/login",
+    SIGNUP: "/signup",
+    VERIFY_OTP: "/verify-otp",  
+     
+} as const;
