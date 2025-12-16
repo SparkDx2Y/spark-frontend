386a9966d219372aee330e2f81d42a618b2e8e59
achuasokan <achuasokan555@gmail.com>
2025-12-16 19:19:49 +0530

    chore: custom error funciton fo rusing in catch abd including react-hook-form

---
+++ src/utils/handleFormError.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import { UseFormSetError, FieldValues, Path } from 'react-hook-form'
+
+
+export function handleFormError<T extends FieldValues>( error: unknown, setError: UseFormSetError<T>, fieldMap?: Record<string, Path<T>>) {
+    let message = "Something went wrong"
+
+    if (axios.isAxiosError(error)) {
+        message = error.response?.data?.message || message;
+      }
+
+      // Map backend messages to fields
+  if (fieldMap) {
+    for (const key in fieldMap) {
+      if (message.toLowerCase().includes(key)) {
+        setError(fieldMap[key], {
+          type: "server",
+          message,
+        });
+        return;
+      }
+    }
+  }
+
+
+  // Fallback → global form error
+  setError("root", {
+    type: "server",
+    message,
+  });
+}
