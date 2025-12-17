eec18ecbe66f1cabefcff4240dc6ed14dcebcb26
achuasokan <achuasokan555@gmail.com>
2025-12-17 06:37:01 +0530

    feat(services): added api of reset and forgot password

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,8 +1,8 @@
 import { api } from "@/lib/axios";
 
 import { AUTH_ENDPOINTS } from "@/constants/api";
-import { LoginPayload, SignupPayload, VerifyOtpPayload } from "@/types/auth/payload";
-import { LoginResponse, ResendOtpResponse, SignupResponse, VerifyOtpResponse } from "@/types/auth/response";
+import { ForgotPasswordPayload, ForgotPasswordVerifyOtpPayload, LoginPayload, ResetPasswordPayload, SignupPayload, VerifyOtpPayload } from "@/types/auth/payload";
+import { ForgotPasswordResponse, ForgotPasswordVerifyOtpResponse, LoginResponse, ResendOtpResponse, ResetPasswordResponse, SignupResponse, VerifyOtpResponse } from "@/types/auth/response";
 
 
 // -----------------------------
@@ -45,3 +45,32 @@ export const login = async (data: LoginPayload): Promise<LoginResponse> => {
   const response = await api.post<LoginResponse>(AUTH_ENDPOINTS.LOGIN, data);
   return response.data;
 };
+
+
+
+// -----------------------------
+// FORGOT PASSWORD
+// -----------------------------
+
+export const forgotPassword = async (data: ForgotPasswordPayload) : Promise<ForgotPasswordResponse> => {
+  const response = await api.post(AUTH_ENDPOINTS.FORGOT_PASSWORD, data);
+  return response.data
+}
+
+// -----------------------------
+// Verify FORGOT PASSWORD
+// -----------------------------
+
+export const verifyForgotPasswordOtp = async(data: ForgotPasswordVerifyOtpPayload) : Promise<ForgotPasswordVerifyOtpResponse> => {
+  const response = await api.post(AUTH_ENDPOINTS.FORGOT_PASSWORD_VERIFY_OTP, data)
+  return response.data
+}
+
+// -----------------------------
+// Reset Password
+// -----------------------------
+
+export const resetPassword = async(data: ResetPasswordPayload): Promise<ResetPasswordResponse> => {
+  const response = await api.post(AUTH_ENDPOINTS.RESET_PASSWORD, data)
+  return response.data
+}
