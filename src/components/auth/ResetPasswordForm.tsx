ba771f8210175961b9e5afad033729934dae7c46
achuasokan <achuasokan555@gmail.com>
2025-12-17 07:08:01 +0530

    feat(resetpasswordpage): implemented the reset password page

--- src/components/auth/ResetPasswordForm.tsx
+++ src/components/auth/ResetPasswordForm.tsx
@@ -1,13 +1,34 @@
+'use client'
+
 import Link from "next/link";
 import Button from "../ui/Button";
 import Input from "../ui/Input";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { resetPasswordSchema, ResetPasswordSchemaType } from "@/validations/auth/reset-password.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { handleFormError } from "@/utils/handleFormError";
+import { resetPassword } from "@/services/authService";
 
 
 
 
 
 export default function ResetPasswordForm() {
+    const router = useRouter()
 
+    const { register, handleSubmit, setError, formState: { errors, isSubmitting}} = useForm<ResetPasswordSchemaType>({
+        resolver: zodResolver(resetPasswordSchema)
+    })
+
+    const onSubmit = async (data: ResetPasswordSchemaType ) => {
+        try {
+            await resetPassword(data)
+            router.push('/login')
+        } catch (error: unknown) {
+            handleFormError(error, setError)
+        }
+    }
     return (
         <div className='space-y-6'>
 
@@ -16,13 +37,17 @@ export default function ResetPasswordForm() {
                 <p className="text-gray-400">Enter your new password</p>
             </div>
 
+            { errors.root?.message && (
+                <p className="text-red-500 text-sm text-center">{errors.root.message}</p>
+            )}
 
-            <form className="space-y-4">
-                <Input label="New Password" type="password" />
 
-                <Input label="Confirm New Password" type="password" />
+            <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+                <Input label="New Password" type="password" {...register('newPassword')} error={errors.newPassword?.message}/>
 
-                <Button>Reset Password</Button>
+                <Input label="Confirm New Password" type="password" {...register('confirmPassword')} error={errors.confirmPassword?.message}/>
+
+                <Button type='submit' disabled={isSubmitting}> {isSubmitting ? "Resetting..." : "Reset Password" }</Button>
             </form>
 
             <p className="text-center text-gray-400 text-sm">
