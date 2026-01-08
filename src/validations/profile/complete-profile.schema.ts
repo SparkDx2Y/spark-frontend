5a7d9e663216549ce20ecb1918cdc9e5a92c3f87
achuasokan <achuasokan555@gmail.com>
2026-01-08 07:46:36 +0530

    fix:add a new validations in the age inthe profile

--- src/validations/profile/complete-profile.schema.ts
+++ src/validations/profile/complete-profile.schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const completeProfileSchema = z.object({
-    age: z.number().int().min(18, "Age must be at least 18"),
+    age: z.number().int().min(18, "Age must be at least 18").max(50, 'Age cannot be more than 100'),
     gender: z.enum(["male", "female"] as const),
     interestedIn: z.enum(["male", "female"] as const),
     profilePhoto: z.string().min(1, "Please upload a profile photo"),
