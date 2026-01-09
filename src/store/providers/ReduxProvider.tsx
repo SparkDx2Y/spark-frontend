ec8de08ad6abf0ced4c94f355928a62ed0dc19a2
achuasokan <achuasokan555@gmail.com>
2026-01-09 13:41:34 +0530

    imported the googleAuthProvider  and cover the authintializer and it will wrap our entire application

--- src/store/providers/ReduxProvider.tsx
+++ src/store/providers/ReduxProvider.tsx
@@ -4,13 +4,18 @@ import { Provider } from 'react-redux';
 import { store } from '../store';
 import { ReactNode } from 'react';
 import AuthInitializer from './AuthInitializer';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function ReduxProvider({ children }: { children: ReactNode }) {
+    const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '';
+
     return (
         <Provider store={store}>
-            <AuthInitializer>
-                {children}
-            </AuthInitializer>
+            <GoogleOAuthProvider clientId={googleClientId}>
+                <AuthInitializer>
+                    {children}
+                </AuthInitializer>
+            </GoogleOAuthProvider>
         </Provider>
     );
 }
