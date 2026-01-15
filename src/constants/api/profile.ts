94f3ef1da12fadc415afd6cb399f548142af8506
achuasokan <achuasokan555@gmail.com>
2026-01-15 10:35:42 +0530

    added new end points and api calling in teh service folder for getAll interest and updateInterest

--- src/constants/api/profile.ts
+++ src/constants/api/profile.ts
@@ -2,6 +2,8 @@ export const PROFILE_ENDPOINTS = {
     COMPLETE_PROFILE: "/profile/complete",
     GET_PROFILE: "/profile/profile",
     UPDATE_PROFILE: "/profile/profile",
+    GET_INTERESTS: "/profile/interests",
+    UPDATE_INTERESTS: "/profile/interests",
 } as const;
 
 
