b2a3f1406f09e41e912f4f612ec869a36c4c9185
achuasokan <achuasokan555@gmail.com>
2026-01-25 02:31:19 +0530

    feat: Introduce `ScrollNav` and `Navbar` components to implement a dynamic, scroll-aware navigation bar.

--- src/components/common/ScrollNav.tsx
+++ src/components/common/ScrollNav.tsx
@@ -14,14 +14,16 @@ export default function ScrollNav({ children }: { children: React.ReactNode }) {
         return () => {
             window.removeEventListener('scroll', handleScroll);
         }
-    },[]);
+    }, []);
 
     return (
-        <nav className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${scrolled
-            ? 'bg-black/80 backdrop-blur-xl shadow-lg shadow-black/50'
-            : 'bg-transparent'
-            }`}>
-                { children }
+        <div className="fixed top-0 left-0 right-0 z-50 flex justify-center py-4">
+            <nav className={`transition-all duration-300 flex items-center justify-between border border-white/10 rounded-full backdrop-blur-3xl ${scrolled
+                ? 'bg-black/50 shadow-lg shadow-primary/5 py-3 px-6 w-[90%] md:w-[70rem]'
+                : 'bg-white/[0.02]  py-3 px-8 w-[95%] md:w-[80rem]'
+                }`}>
+                {children}
             </nav>
+        </div>
     )
 }
