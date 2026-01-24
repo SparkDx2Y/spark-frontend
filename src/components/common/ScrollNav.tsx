70db328ebdfed5e8ddf1f70c37d0adcc8f3370e3
achuasokan <achuasokan555@gmail.com>
2026-01-25 02:45:02 +0530

    changed some css errors in the page

--- src/components/common/ScrollNav.tsx
+++ src/components/common/ScrollNav.tsx
@@ -19,8 +19,8 @@ export default function ScrollNav({ children }: { children: React.ReactNode }) {
     return (
         <div className="fixed top-0 left-0 right-0 z-50 flex justify-center py-4">
             <nav className={`transition-all duration-300 flex items-center justify-between border border-white/10 rounded-full backdrop-blur-3xl ${scrolled
-                ? 'bg-black/50 shadow-lg shadow-primary/5 py-3 px-6 w-[90%] md:w-[70rem]'
-                : 'bg-white/[0.02]  py-3 px-8 w-[95%] md:w-[80rem]'
+                ? 'bg-black/50 shadow-lg shadow-primary/5 py-3 px-6 w-[90%] md:w-280'
+                : 'bg-white/2  py-3 px-8 w-[95%] md:w-7xl'
                 }`}>
                 {children}
             </nav>
